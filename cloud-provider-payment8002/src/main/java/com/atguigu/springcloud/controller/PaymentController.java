852e11c9c457ed73a6f9381175460b61d5809c41
longhua.shen <>
2022-06-27 20:17:02 +0800

    init

---
+++ cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,81 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.service.PaymentService;
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * (Payment)表控制层
+ *
+ * @author makejava
+ * @since 2020-03-06 14:22:26
+ */
+@RestController
+@Slf4j
+@RequestMapping("payment")
+public class PaymentController {
+    /**
+     * 服务对象
+     */
+    @Resource
+    private PaymentService paymentService;
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+
+    /**
+     * 通过主键查询单条数据
+     *
+     * @param id 主键
+     * @return 单条数据
+     */
+    @GetMapping("selectOne/{id}")
+    public CommonResult<Payment> selectOne(@PathVariable("id") Long id) {
+        Payment payment = this.paymentService.queryById(id);
+        log.info("***************查询结果："+payment+"\t"+"O（U_U）O哈哈~");
+        if(payment!=null){
+            return new CommonResult<Payment>(200,"查询成功，serverPort: "+serverPort,payment);
+        }else{
+            return new CommonResult<Payment>(444,"没有对应记录,查询ID： "+id,null);
+        }
+    }
+
+    @PostMapping("create")
+    public CommonResult create(@RequestBody Payment payment) {
+        Payment insert = this.paymentService.insert(payment);
+        log.info("*************插入结果："+insert);
+        if(insert!=null){
+            return new CommonResult(200,"插入数据库成功" ,insert);
+        }else{
+            return new CommonResult(444,"插入数据库失败" ,null);
+        }
+    }
+
+    @GetMapping("discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        services.forEach(service->{
+            System.out.println("---service-->"+service);
+        });
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        instances.forEach(instance->{
+            System.out.println(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        });
+        return this.discoveryClient;
+    }
+
+    @GetMapping("lb")
+    public String getPaymentLB() {
+        return serverPort;
+    }
+}
